728be3cd0b7b19f3d9d3736b7a71e1e9b4805616
ilshat5ot <ilshat5ot@gmail.com>
2023-09-22 12:30:50 +0300

    добавлен метод проверки существования пользователя

---
+++ auth-service/src/main/java/ru/sadykov/controller/UserController.java
@@ -0,0 +1,28 @@
+package ru.sadykov.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.sadykov.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/user")
+
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    //Правильно ли возвращать просто булин выражение. Или это должно быть dto с полем которое содержит значение булин.
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> userExists(@PathVariable Long userId) {
+        boolean userExists = userService.userExists(userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userExists);
+    }
+}
